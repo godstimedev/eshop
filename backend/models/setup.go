1f7c4e4cfe56695d631133ed3b2265401b82111c
olupy <fortuneosho@gmail.com>
2023-04-12 16:37:30 +0100

    registration done

--- backend/models/setup.go
+++ backend/models/setup.go
@@ -8,15 +8,16 @@ import (
 )
 
 var DB *gorm.DB
+var err error
 
 func ConnectDataBase() {
-	DB, err := gorm.Open("sqlite3", "test.db")
+	DB, err = gorm.Open("sqlite3", "test.db")
 	if err != nil {
 		fmt.Println(err.Error())
 		panic("failed to connect database")
 	}
 	fmt.Print("connected successfully")
-	defer DB.Close()
+	//defer DB.Close()
 
 	// Migrate the schema
 	DB.AutoMigrate(&User{})
